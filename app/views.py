23afd73b831157f0a769c501586dcda1ffcb7afe
yar_shto <jarik13012000@gmail.com>
2025-04-26 12:50:02 +0100

    openai

--- app/views.py
+++ app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
+from .forms import PromptForm
+
+import openai
+openai.api_key = ''
+
 # Create your views here.
+def home(request):
+    response_text = None
+    response_image = None
+
+    if request.method == 'POST':
+        form = PromptForm(request.POST)
+        ## is_valid
+        if form.is_valid():
+            prompt = form.cleaned_data['prompt']
+
+
+            completion = openai.OpenAI().chat.completions.create(
+                model="gpt-4.1",
+                messages=[
+                    {"role": "developer", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt}
+                ]
+            )
+
+            response_text = completion.choices[0].message
+
+    else:
+        form = PromptForm()
+
+    return render(request, 'home.html', {
+        'form':form,
+        'response_text':response_text,
+        'response_image':response_image,
+    })
